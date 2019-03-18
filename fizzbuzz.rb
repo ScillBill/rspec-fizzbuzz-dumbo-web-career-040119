fada12af88c93418ac8742ec9a861d5f252243b8
Priscilla Vazquez <scillamae922@gmail.com>
2019-03-18 01:45:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-numbers = (1..90)
+
 def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
     puts "FizzBuzz"
